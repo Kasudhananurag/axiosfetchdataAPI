8c3293b2a1a98e4ce302cc9b368acb631eccce4f
Kasudhananurag <114092146+Kasudhananurag@users.noreply.github.com>
2022-10-14 17:09:01 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -6,12 +6,16 @@ import Axios from "axios";
 function App() {
   const [catFact, setCatFact] = useState("");
   {/*fetch("https://catfact.ninja/fact").then((res) => res.json() ).then((data) => {console.log(data)}); */}
+  const fetchCatFact =() =>
+  {
+    Axios.get("https://catfact.ninja/fact").then((res) => {setCatFact(res.data.fact)});
+  }
   useEffect(() =>{
     Axios.get("https://catfact.ninja/fact").then((res) => {setCatFact(res.data.fact)})
   }, []);
   return (
     <div className="App">
-      <button>Generate cat fact</button>
+      <button onClick={fetchCatFact}>Generate cat fact</button>
       <p> {catFact} </p>
     </div>
   );
